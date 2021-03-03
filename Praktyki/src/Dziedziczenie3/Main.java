38d1421c88d1f699d2332b0c8e4c3f77100b846b
BartoszDiering <bartekdiering@wp.pl>
2021-03-03 18:19:08 +0100

    Create Main.java

---
+++ Praktyki/src/Dziedziczenie3/Main.java
@@ -0,0 +1,9 @@
+package Dziedziczenie3;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Spawacz spawacz = new Spawacz(2000, "pełny", true, true);
+        spawacz.wykonujePrace();
+    }
+}
