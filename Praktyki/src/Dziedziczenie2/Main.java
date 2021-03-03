71eae99e783aa9f328680129066b9c0c1ea4cc50
BartoszDiering <bartekdiering@wp.pl>
2021-03-03 18:19:18 +0100

    Create Main.java

---
+++ Praktyki/src/Dziedziczenie2/Main.java
@@ -0,0 +1,11 @@
+package Dziedziczenie2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pies pies = new Pies(4, 4, "Azor", "czarna", 12);
+
+
+        pies.dajGłos();
+    }
+}
