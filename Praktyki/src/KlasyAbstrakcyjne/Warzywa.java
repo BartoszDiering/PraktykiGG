24aa6e3dd58f8102593810191bec88183323734f
BartoszDiering <bartekdiering@wp.pl>
2021-03-06 13:58:18 +0100

    Create Warzywa.java

---
+++ Praktyki/src/KlasyAbstrakcyjne/Warzywa.java
@@ -0,0 +1,16 @@
+package KlasyAbstrakcyjne;
+
+abstract public class Warzywa {
+    abstract public void czerwony();
+    abstract public void zolty();
+
+
+    public void korzeniowe()
+    {
+        System.out.println("Warzywo korzeniowe");
+    }
+    public void owocowe()
+    {
+        System.out.println("Warzywo owocowe");
+    }
+}
