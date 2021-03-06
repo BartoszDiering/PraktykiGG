b5f8ed9f71ba39ee0c3d07c5c6acd3f6b6555600
BartoszDiering <bartekdiering@wp.pl>
2021-03-06 13:57:49 +0100

    Create KolorWarzywa.java

---
+++ Praktyki/src/KlasyAbstrakcyjne/KolorWarzywa.java
@@ -0,0 +1,12 @@
+package KlasyAbstrakcyjne;
+
+public class KolorWarzywa extends Warzywa {
+    @Override
+    public void czerwony() {
+        System.out.println("Czerwone warzywo");
+    }
+    @Override
+    public void zolty() {
+        System.out.println("Zolte warzywo");
+    }
+    }
