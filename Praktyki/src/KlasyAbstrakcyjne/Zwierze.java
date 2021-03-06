c09fd72f6187900dc99f3123d58bc0122c6b00ee
BartoszDiering <bartekdiering@wp.pl>
2021-03-06 13:58:21 +0100

    Create Zwierze.java

---
+++ Praktyki/src/KlasyAbstrakcyjne/Zwierze.java
@@ -0,0 +1,12 @@
+package KlasyAbstrakcyjne;
+
+abstract public class Zwierze {
+    abstract public void wiek();
+    abstract public void nazwa();
+    public void SprawdzCzyDajeGlos(){
+        System.out.println("Miau");
+        }
+    public void sprawdzCzySpi(){
+        System.out.println("Zzz");
+        }
+        }
